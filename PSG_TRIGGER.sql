0782a1ff52bf7d6c274307d6756cf5aa86aeddea
StuartLBrown <stulwbrown@gmail.com>
2021-03-27 21:26:09 -0700

    added test statements for the triggers

--- PSG_TRIGGER.sql
+++ PSG_TRIGGER.sql
@@ -60,3 +60,29 @@ FOR EACH ROW
 WHEN (OLD.likecount < NEW.likecount)
 EXECUTE PROCEDURE UpdateTotalTipLikes();
 
+-- Testing UpdateTipCount and UpdateNumTips
+--SELECT businessid, userid, numoftips, tipcount, totaltiplikes FROM Business, UserTable  WHERE businessid = '2D3gx261cReZb-K1RZJA_g' and userid = '--5XzJ2pRsVVJiJUfzZlgQ';
+
+--INSERT INTO tip VALUES('2000-01-01',0,'test','--5XzJ2pRsVVJiJUfzZlgQ','2D3gx261cReZb-K1RZJA_g');
+
+--SELECT businessid, userid, numoftips, tipcount, totaltiplikes FROM Business, UserTable  WHERE businessid = '2D3gx261cReZb-K1RZJA_g' and userid = '--5XzJ2pRsVVJiJUfzZlgQ';
+
+
+-- Testing UpdateNumCheckins
+-- SELECT businessid, numofcheckin FROM Business WHERE businessid = 'Ik-vXB7HCwnC44KDf_nwdw';
+
+-- INSERT INTO checkins VALUES ('2010-01-01 09:30:30','Ik-vXB7HCwnC44KDf_nwdw');
+
+-- SELECT businessid, numofcheckin FROM Business WHERE businessid = 'Ik-vXB7HCwnC44KDf_nwdw';
+
+-- Testing UpdateTotalTipLikes()
+
+
+-- Testing UpdateTotalTipLikes
+
+-- SELECT userid,totaltiplikes FROM usertable WHERE userid = '---1lKK3aKOuomHnwAkAow';
+
+-- UPDATE tip SET likecount = likecount+1 WHERE userid = '---1lKK3aKOuomHnwAkAow' and textoftip = 'Phone number should be 1531';
+
+-- SELECT userid,totaltiplikes FROM usertable WHERE userid = '---1lKK3aKOuomHnwAkAow';
+
