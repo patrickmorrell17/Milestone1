f0a952cb47cc0b282b501681b6f43eab9c7df294
StuartLBrown <stulwbrown@gmail.com>
2021-04-11 17:30:50 -0700

    distance function is done, will tweak in the future such that it can be called w/ either the raw coordinates or the id's of the business and user

---
+++ PSG_DISTANCE.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE FUNCTION haversine (lat1 float,lon1 float,lat2 float,lon2 float) RETURNS float as $$
+	DECLARE
+		dlatitude float:= (lat2-lat1)*pi()/180.0;
+		dlongitude float:= (lon2-lon1)*pi()/180.0;
+		tempvalue float;
+		radiusofearth float := 6371;
+	BEGIN
+		-- computing distances
+		-- converting to radians
+		lat1:=(lat1)*pi()/180.0;
+		lat2:=(lat2)*pi()/180.0;
+		
+		-- applying formula
+		tempvalue :=(power(sin(dlatitude/2),2)+power(sin(dlongitude/2),2)*cos(lat1)*cos(lat2));
+		tempvalue:=2*asin(sqrt(tempvalue));
+		RETURN tempvalue*radiusofearth;
+	END;
+$$ LANGUAGE plpgsql;
