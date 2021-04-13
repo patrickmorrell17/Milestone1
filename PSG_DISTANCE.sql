070a84b5d455a76e1a0bd2a5310161efa87b91b8
StuartLBrown <stulwbrown@gmail.com>
2021-04-13 15:01:24 -0700

    misc comments

--- PSG_DISTANCE.sql
+++ PSG_DISTANCE.sql
@@ -1,11 +1,14 @@
 CREATE OR REPLACE FUNCTION haversine (lat1 float,lon1 float,lat2 float,lon2 float) RETURNS float as $$
 	DECLARE
+		-- getting the distance between the 2 latitudes and converting to radians
 		dlatitude float:= (lat2-lat1)*pi()/180.0;
+		-- getting the distance between the 2 longitudes and converting to radians
 		dlongitude float:= (lon2-lon1)*pi()/180.0;
+		-- user to make formula smaller
 		tempvalue float;
+		-- radius of earth in meters
 		radiusofearth float := 6371;
 	BEGIN
-		-- computing distances
 		-- converting to radians
 		lat1:=(lat1)*pi()/180.0;
 		lat2:=(lat2)*pi()/180.0;
@@ -17,14 +20,16 @@ CREATE OR REPLACE FUNCTION haversine (lat1 float,lon1 float,lat2 float,lon2 floa
 	END;
 $$ LANGUAGE plpgsql;
 
-
+-- overloaded function, takes only the userid and the businessid and computes the distance between them
 CREATE OR REPLACE FUNCTION haversine (useid VARCHAR,busid VARCHAR) RETURNS float as $$
 	DECLARE
+		-- getting all the values
 		businesslat float:=(SELECT latitude FROM Business WHERE businessid = busid);
 		businesslong float:=(SELECT longitude FROM Business WHERE businessid = busid);
 		userlat float:=(SELECT latitude FROM UserTable WHERE userid = useid);
 		userlong float:=(SELECT longitude FROM UserTable WHERE userid = useid);
 	BEGIN
+		-- calling original function
 		RETURN haversine(userlat,userlong,businesslat,businesslong);
 	END;
 $$ LANGUAGE plpgsql;
